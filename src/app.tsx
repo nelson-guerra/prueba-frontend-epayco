8188c4288f02ae11fe189d5bbb189f3ba22c3e03
Nelson Guerra <nelson@a9-24.local>
2025-06-03 01:43:09 -0500

    fix: remove tanstack query and its hooks

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,5 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { PostPage } from "./pages/post/PostPage";
 
-const queryClient = new QueryClient();
-
 export const App = () => {
-   return (
-      <QueryClientProvider client={queryClient}>
-         <PostPage />
-      </QueryClientProvider>
-   );
+   return <PostPage />;
 };
--- src/components/organisms/PostForm/PostFormOrganism.tsx
+++ src/components/organisms/PostForm/PostFormOrganism.tsx
@@ -4,7 +4,6 @@ import { addPost } from "../../../redux/states/post";
 import { Post } from "../../../types/index";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
-
 import { InputAtom } from "../../atoms/Input/InputAtom";
 import { TextAreaAtom } from "../../atoms/TextArea/TextAreaAtom";
 import { ButtonAtom } from "../../atoms/Button/ButtonAtom";
@@ -40,48 +39,46 @@ export const PostFormOrganism = () => {
    };
 
    return (
-      <>
-         <div className="flex min-h-full flex-col pt-6 pb-12">
-            <h2 className="text-center text-2xl/9 font-bold tracking-tight text-gray-900">
-               New Item
-            </h2>
-            <div className="mt-2 sm:mt-6 sm:mx-auto">
-               <form
-                  onSubmit={handleSubmit(onSubmit)}
-                  className="flex sm:flex-row flex-col space-y-6 gap-x-2"
-               >
-                  <div className="mb-0">
-                     <label className="block text-sm/6 font-medium text-gray-900">
-                        Title
-                     </label>
-                     <div className="mt-1">
-                        <InputAtom
-                           errors={errors}
-                           label="title"
-                           register={register}
-                           required
-                        />
-                     </div>
+      <div className="flex min-h-full flex-col pt-6 pb-12">
+         <h2 className="text-center text-2xl/9 font-bold tracking-tight text-gray-900">
+            New Item
+         </h2>
+         <div className="mt-2 sm:mt-6 sm:mx-auto">
+            <form
+               onSubmit={handleSubmit(onSubmit)}
+               className="flex sm:flex-row flex-col space-y-6 gap-x-2"
+            >
+               <div className="mb-0">
+                  <label className="block text-sm/6 font-medium text-gray-900">
+                     Title
+                  </label>
+                  <div className="mt-1">
+                     <InputAtom
+                        errors={errors}
+                        label="title"
+                        register={register}
+                        required
+                     />
                   </div>
-                  <div className="mb-0">
-                     <label className="block text-sm/6 font-medium text-gray-900">
-                        Description
-                     </label>
-                     <div className="mt-1">
-                        <TextAreaAtom
-                           errors={errors}
-                           label="body"
-                           register={register}
-                           required
-                        />
-                     </div>
+               </div>
+               <div className="mb-0">
+                  <label className="block text-sm/6 font-medium text-gray-900">
+                     Description
+                  </label>
+                  <div className="mt-1">
+                     <TextAreaAtom
+                        errors={errors}
+                        label="body"
+                        register={register}
+                        required
+                     />
                   </div>
-                  <div className="flex items-center mt-4 sm:mt-1.5 flex-none">
-                     <ButtonAtom type="submit">Add Item</ButtonAtom>
-                  </div>
-               </form>
-            </div>
+               </div>
+               <div className="flex items-center mt-4 sm:mt-1.5 flex-none">
+                  <ButtonAtom type="submit">Add Item</ButtonAtom>
+               </div>
+            </form>
          </div>
-      </>
+      </div>
    );
 };
